c381bfc2138abe5c95dcbd3933edf1027dfe5df3
Cannon Lewis <cannon10100@gmail.com>
2019-08-06 20:11:29 -0500

    Created image views, skeleton for pipeline

--- main.cpp
+++ main.cpp
@@ -70,6 +70,7 @@ private:
   std::vector<VkImage> swapChainImages_;
   VkFormat swapChainImageFormat_;
   VkExtent2D swapChainExtent_;
+  std::vector<VkImageView> swapChainImageViews_;
 
   VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
     for (const auto& availableFormat : availableFormats) {
@@ -409,6 +410,39 @@ private:
     pickPhysicalDevice();
     createLogicalDevice();
     createSwapChain();
+    createImageViews();
+    createGraphicsPipeline();
+  }
+
+  void createGraphicsPipeline() {
+
+  }
+
+  void createImageViews() {
+    swapChainImageViews_.resize(swapChainImages_.size());
+
+    for (size_t i = 0; i < swapChainImages_.size(); i++) {
+      VkImageViewCreateInfo createInfo = {};
+      createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+      createInfo.image = swapChainImages_[i];
+      createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+      createInfo.format = swapChainImageFormat_;
+
+      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+      createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+      createInfo.subresourceRange.baseMipLevel = 0;
+      createInfo.subresourceRange.levelCount = 1;
+      createInfo.subresourceRange.baseArrayLayer = 0;
+      createInfo.subresourceRange.layerCount = 1;
+
+      if (vkCreateImageView(device_, &createInfo, nullptr, &swapChainImageViews_[i]) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create image views!"); 
+      }
+    }
   }
 
   void createSwapChain() {
@@ -472,6 +506,10 @@ private:
   }
 
   void cleanup() {
+    for (auto imageView : swapChainImageViews_) {
+      vkDestroyImageView(device_, imageView, nullptr);
+    }
+
     vkDestroySwapchainKHR(device_, swapChain_, nullptr);
     vkDestroyDevice(device_, nullptr);
 
